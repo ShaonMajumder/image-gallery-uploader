7356169c1d495bf26e041445a899c3cfafc7c222
Shaon Majumder <shaon.majumder@byslglobal.com>
2023-06-05 17:36:59 +0600

    package cleaned

---
+++ build/MessageConst.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.WARNING = exports.SUCCESS = exports.INFO = exports.ERROR = void 0;
+var SUCCESS = 'SUCCESS';
+exports.SUCCESS = SUCCESS;
+var ERROR = 'ERROR';
+exports.ERROR = ERROR;
+var INFO = 'INFO';
+exports.INFO = INFO;
+var WARNING = 'WARNING';
+exports.WARNING = WARNING;
