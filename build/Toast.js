0da9b5afd75a94638ae089df71989d336030faf9
Shaon Majumder <shaon.majumder@byslglobal.com>
2023-06-05 17:53:57 +0600

    Test

--- build/Toast.js
+++ build/Toast.js
@@ -1,42 +1,38 @@
-"use strict";
+import {toast} from "react-toastify";
+import {ERROR, INFO, SUCCESS, WARNING} from "./MessageConst";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.notify = void 0;
-var _reactToastify = require("react-toastify");
-var _MessageConst = require("./MessageConst");
-var notify = function notify(message, type) {
-  switch (type) {
-    case _MessageConst.SUCCESS:
-      _reactToastify.toast.success(message, {
-        position: _reactToastify.toast.POSITION.TOP_RIGHT,
-        theme: "colored"
-      });
-      break;
-    case _MessageConst.ERROR:
-      _reactToastify.toast.error(message, {
-        position: _reactToastify.toast.POSITION.TOP_RIGHT,
-        theme: "colored"
-      });
-      break;
-    case _MessageConst.INFO:
-      _reactToastify.toast.info(message, {
-        position: _reactToastify.toast.POSITION.TOP_RIGHT,
-        theme: "colored"
-      });
-      break;
-    case _MessageConst.WARNING:
-      _reactToastify.toast.warn(message, {
-        position: _reactToastify.toast.POSITION.TOP_RIGHT,
-        theme: "colored"
-      });
-      break;
-    default:
-      _reactToastify.toast.info(message, {
-        position: _reactToastify.toast.POSITION.TOP_RIGHT,
-        theme: "colored"
-      });
-  }
-};
-exports.notify = notify;
+export const notify = (message, type) => {
+
+    switch (type) {
+        case SUCCESS:
+            toast.success(message, {
+                position: toast.POSITION.TOP_RIGHT,
+                theme: "colored",
+            });
+            break;
+        case ERROR:
+            toast.error(message, {
+                position: toast.POSITION.TOP_RIGHT,
+                theme: "colored",
+            });
+            break;
+        case INFO:
+            toast.info(message, {
+                position: toast.POSITION.TOP_RIGHT,
+                theme: "colored",
+            });
+            break;
+        case WARNING:
+            toast.warn(message, {
+                position: toast.POSITION.TOP_RIGHT,
+                theme: "colored",
+            });
+            break;
+        default:
+            toast.info(message, {
+                position: toast.POSITION.TOP_RIGHT,
+                theme: "colored",
+            });
+    }
+
+}
